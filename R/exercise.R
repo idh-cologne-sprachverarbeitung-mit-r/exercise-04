3766a1a0d1c184eea4b22b3188021ac68b3c793d
ariannabienati <abienati@smail.uni-koeln.de>
2020-05-20 14:51:36 +0200

    first attempt

--- R/exercise.R
+++ R/exercise.R
@@ -4,10 +4,46 @@ set.seed(1)
 # generate a vector of 1000 random numbers
 vec <- runif(1000)
 
+a <- matrix(vec, 250, 4)
 
+b <- t(a)
 
+c <- colSums(a)
 
+d <- (colSums(b))[210]
+
+vec1 <- "Hello World!"
+vec2 <- TRUE
+vec3 <- c(3.5)
+
+e <- list(vec1, vec2, vec3)
+
+f <- list (1, 2, 3)
+
+names(f) <- c("f1", "f2", "f3")
+
+g <- f$f2
+
+f$f4 <- a
+
+h <- f$f4
+
+name <- c("Paul", "Thomas")
+age <- c(60, 17)
+address <- c("Rudolfstr. 27", "Friesenplatz 1")
+knowsR <- c(FALSE, TRUE)
+
+i <- data.frame(name, age, address, knowsR)
+attach(i)
+
+mean(age) -> j
+
+k <- i[2,]
 
 words <- c("the", "dog", "barks", ".")
 pos <- c("det", "nn", "vbz", "punct")
 lemma <- c("the", "dog", "bark", ".")
+length <- nchar(words)
+
+m <- data.frame (words, pos, lemma, length)
+
