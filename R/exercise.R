7166f6be3f663a1dc9e6fd66ad81477bffa54538
chrkell <you@example.com>
2020-05-22 16:11:06 +0200

    6

--- R/exercise.R
+++ R/exercise.R
@@ -32,9 +32,8 @@ f <- list(f1=1, f2=2, f3=3)
 g <- f[[2]]
 
 # h)
-h <- f
-h[["f4"]] <- a
-f <- h
+# h <- f
+# h[["f4"]] <- a
 
 # i)
 i <- data.frame("Name"=c("Anna","Luis"), "Age"=c(2, 20), "Address"=c("Mohnstr. 7","Kölner Str. 1"), "knowsR"=c(FALSE,TRUE))
